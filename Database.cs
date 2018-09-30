e3233cd1fe72c1ed8bd7ffb2a777dea374a20e46
Valdas Rakutis <valdas@rakutis.lt>
2018-09-30 14:26:26 +0300

    working db prototype

---
+++ Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+public class PlateDatabase : IEnumerable<string>
+{
+    public PlateDatabase()
+    {
+
+    }
+
+    public string this[int index]
+    {
+        get { return null; }
+        set { }
+    }
+
+    System.Collections.IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    IEnumerator<string> GetEnumerator()
+    {
+        //foreach var in db yield return var
+        yield return "NaN";
+    }
+
+}
---
+++ epicenterWin/PlateDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class PlateDatabase : IEnumerable<string>
+{
+    private readonly string[] _plates =
+    {
+        "ABC123",
+        "DEF456",
+    };
+
+    public PlateDatabase()
+    {
+    }
+
+    public string this[int index]
+    {
+        get { return _plates[index]; }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    IEnumerator<string> GetEnumerator()
+    {
+        foreach (string str in _plates)
+            yield return str;
+    }
+
+    IEnumerator<string> IEnumerable<string>.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
