2038e799597e923073ed978f1e5043c6c3c2c0f0
Žygimantas Stongvilas <stongvilas.zygimantas@gmail.com>
2018-11-22 02:51:00 +0200

    initial face recognition phase

---
+++ backend/WebApplication1/Services/FaceService.cs
@@ -0,0 +1,219 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Services
+{
+    public class FaceService
+    {
+        private const string _subscriptionKey = "822fc0c2b7704dd48003c050650f4522";
+        private const string _uriBase = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/";
+        private const string _filePath = @"C:\Users\stong\Desktop\TOP\imgs\img8.jpg";
+
+        public static async void MakeAnalysisRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _subscriptionKey);
+
+            // Request parameters. A third optional parameter is "details".
+            string requestParameters = "returnFaceId=true&returnFaceLandmarks=false" +
+                "&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses," +
+                "emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+            // Assemble the URI for the REST API Call.
+            string uri = _uriBase + "detect?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(_filePath);
+
+            Console.WriteLine(uri);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json"
+                // and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n");
+                Console.WriteLine(JsonPrettyPrint(contentString));
+                Console.WriteLine("\nPress Enter to exit...");
+            }
+        }
+
+        public static async void PersonGroupCreate(string groupID)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _subscriptionKey);
+
+            // Assemble the URI for the REST API Call.
+            string uri = _uriBase + "persongroups/{groupID}";
+            Console.WriteLine(uri);
+
+            HttpResponseMessage response;
+
+            PersonGroupRequest pGroup = new PersonGroupRequest()
+            {
+                Name = "groupName",
+                UserData = "userDefinedData"
+            };
+
+            string contentText = JsonConvert.SerializeObject(pGroup);
+
+            using (StringContent content = new StringContent(contentText))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                // Execute the REST API call.
+                response = await client.PutAsync(uri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n");
+                Console.WriteLine(JsonPrettyPrint(contentString));
+                Console.WriteLine("\nPress Enter to exit...");
+            }
+        }
+
+        public static async void PersonCreate(string groupID)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _subscriptionKey);
+
+            // Assemble the URI for the REST API Call.
+            string uri = _uriBase + "persongroups/{groupID}/persons";
+            Console.WriteLine(uri);
+
+            HttpResponseMessage response;
+
+            PersonRequest person = new PersonRequest()
+            {
+                Name = "personName",
+                UserData = "userDefinedPersonData"
+            };
+
+            string contentText = JsonConvert.SerializeObject(person);
+
+            using (StringContent content = new StringContent(contentText))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n");
+                Console.WriteLine(JsonPrettyPrint(contentString));
+                Console.WriteLine("\nPress Enter to exit...");
+            }
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+
+    public class PersonGroupRequest
+    {
+        public string Name { get; set; }
+        public string UserData { get; set; }
+    }
+
+    public class PersonRequest
+    {
+        public string Name { get; set; }
+        public string UserData { get; set; }
+    }
+}
