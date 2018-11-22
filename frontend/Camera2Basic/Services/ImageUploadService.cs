02fce12ff912f9b21d82b6f58aca2685d64f9cbb
Valdas Rakutis <valdas@rakutis.lt>
2018-11-22 22:06:53 +0200

    Add front-end to dev.

---
+++ frontend/Camera2Basic/Services/ImageUploadService.cs
@@ -0,0 +1,32 @@
+﻿using Android.Media;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace Camera2Basic.Services
+{
+    public class ImageUploadService
+    {
+        private readonly string ServiceURL = @"https://www.google.com";
+
+        public async void Upload(Image image)
+        {
+            RestClient client = new RestClient(ServiceURL);
+
+            RestRequest request = new RestRequest("", Method.GET);
+            RestResponse response = await GetResponseContentAsync(client, request) as RestResponse;
+            //TODO: actually fill these in and use them
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+        }
+
+        private Task<IRestResponse> GetResponseContentAsync(RestClient client, RestRequest request)
+        {
+            var taskCompletionSource = new TaskCompletionSource<IRestResponse>();
+            client.ExecuteAsync(request, response => {
+                taskCompletionSource.SetResult(response);
+            });
+            return taskCompletionSource.Task;
+        }
+    }
+}
