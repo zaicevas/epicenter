fc3b24a4f276fe72f7626bf2ddaf1a7438c7266b
Tomas Zaicevas <tozaicevas@gmail.com>
2018-11-22 02:26:53 +0200

    dependency injection + app config

---
+++ backend/WebApplication1/Mappers/Mapper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Mappers
+{
+    public class Mapper
+    {
+        private readonly AppSettings _appSettings;
+
+        public Mapper(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+    }
+}
--- backend/WebApplication1/Startup.cs
+++ backend/WebApplication1/Startup.cs
@@ -9,22 +9,35 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApplication1.Mappers;
+using WebApplication1.Models;
 
 namespace WebApplication1
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            // these are injected by default (from Program.cs)
+            Configuration = configuration;
+            HostingEnvironment = env;
+
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.Configure<AppSettings>(Configuration.GetSection("ApplicationSettings"));
+            /*
+             *  to inject raw Configuration
+            services.AddSingleton(Configuration);
+                after that you can do smth like 
+            var environment = Configuration["ConnectionString:Default"];
+            */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
