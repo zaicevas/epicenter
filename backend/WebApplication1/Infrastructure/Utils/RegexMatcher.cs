d76a2e0859d228dff0882a8fadcf23454bcfce2c
Tomas Zaicevas <tozaicevas@gmail.com>
2018-11-22 22:04:11 +0200

    default controller

---
+++ backend/WebApplication1/Infrastructure/Utils/RegexMatcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Infrastructure.Utils
+{
+    public static class RegexMatcher
+    {
+        private static Regex _ltPlate = new Regex(@"^[A-z]{3}-?\d{3}$");
+
+        public static bool NumberPlateValid(this string str)
+        {
+            return _ltPlate.IsMatch(str);
+        }
+    }
+}
