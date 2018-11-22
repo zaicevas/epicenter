81c782fdeb577abc7be819ce5bdd56569dd0cf40
Valdas Rakutis <valdas@rakutis.lt>
2018-11-23 00:34:21 +0200

    add .sln for the project

--- frontend/Camera2Basic/CameraActivity.cs
+++ frontend/Camera2Basic/CameraActivity.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace Camera2Basic
--- frontend/Camera2Basic/Services/ImageUploadService.cs
+++ frontend/Camera2Basic/Services/ImageUploadService.cs
@@ -8,16 +8,18 @@ namespace Camera2Basic.Services
 {
     public class ImageUploadService
     {
+
         private readonly string ServiceURL = @"https://www.google.com";
 
-        public async void Upload(Image image)
+        //TODO fill
+        public async Task<JObject> Upload(Image image)
         {
             RestClient client = new RestClient(ServiceURL);
 
             RestRequest request = new RestRequest("", Method.GET);
             RestResponse response = await GetResponseContentAsync(client, request) as RestResponse;
-            //TODO: actually fill these in and use them
-            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+
+            return JsonConvert.DeserializeObject<JObject>(response.Content);
         }
 
         private Task<IRestResponse> GetResponseContentAsync(RestClient client, RestRequest request)
