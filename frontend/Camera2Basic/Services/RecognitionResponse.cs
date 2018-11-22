b7781fcc65621411a0cad830a6830b5b67a554b0
Valdas Rakutis <valdas@rakutis.lt>
2018-11-23 01:35:19 +0200

    Add RecognitionResponce struct for json deserialization.

---
+++ frontend/Camera2Basic/Services/RecognitionResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Camera2Basic.Services
+{
+    public struct RecognitionResponse
+    {
+        public bool Recognized { get; set; }
+        public string Message { get; set; }
+    }
+}
