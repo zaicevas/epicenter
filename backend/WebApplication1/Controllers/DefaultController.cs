0079162f9a7fa43d6e827ef816def0d79e6b19c9
Žygimantas Stongvilas <stongvilas.zygimantas@gmail.com>
2018-11-23 02:56:36 +0200

    Controller fix

--- backend/WebApplication1/Controllers/DefaultController.cs
+++ backend/WebApplication1/Controllers/DefaultController.cs
@@ -12,19 +12,12 @@ namespace WebApplication1.Controllers
         [HttpPost]
         [ProducesResponseType(200, Type = typeof(string))]
         [ProducesResponseType(404)]
-        public async System.Threading.Tasks.Task<IActionResult> PostAsync([FromBody] string value)
+        public IActionResult Post([FromBody] string value)
         {
-            byte[] imageArray = System.IO.File.ReadAllBytes(@"C:\Users\stong\Desktop\TOP\imgs\Obama5.jpg");
+            byte[] imageArray = System.IO.File.ReadAllBytes(@"C:\Users\ferN\plate_testing\FNN883.jpg");
             string base64ImageRepresentation = Convert.ToBase64String(imageArray);
-<<<<<<< HEAD
-            PersonResponse personResponse = await new FaceService().RecognizeAsync(base64ImageRepresentation);
-            PlateResponse plateResponse = new PlateService().Recognize(base64ImageRepresentation);
-            System.Diagnostics.Debug.WriteLine(plateResponse.Message);
-            System.Diagnostics.Debug.WriteLine(personResponse.Message);
-=======
             //PlateResponse plateResponse = new PlateService().Recognize("noob");
             //System.Diagnostics.Debug.WriteLine(plateResponse.Message);
->>>>>>> 5dfd2f85f9a2f518aa102721ffd13ccdd3becdb3
             return Ok("ALL GOOD BOY");
         }
     }
