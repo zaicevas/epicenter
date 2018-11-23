7a30881f592e27897c9f202444199f764b2c0da7
Žygimantas Stongvilas <stongvilas.zygimantas@gmail.com>
2018-11-23 07:57:30 +0200

    Renamed async methods

--- backend/WebApplication1/Services/FaceService.cs
+++ backend/WebApplication1/Services/FaceService.cs
@@ -22,7 +22,7 @@ namespace WebApplication1.Services
 
         public async Task<PersonResponse> RecognizeAsync(string base64)
         {
-            List<Person> recognizedPersons = await CallFaceAPI(Convert.FromBase64String(base64));
+            List<Person> recognizedPersons = await CallFaceAPIAsync(Convert.FromBase64String(base64));
             string message = string.Empty;
             bool recognized = recognizedPersons != null && recognizedPersons.Count > 0;
             if (recognized)
@@ -39,10 +39,10 @@ namespace WebApplication1.Services
             return new PersonResponse { Recognized = recognized, Message = message };
         }
 
-        private async Task<List<Person>> CallFaceAPI(byte[] image)
+        private async Task<List<Person>> CallFaceAPIAsync(byte[] image)
         {
             List<Person> recognizedPersons = new List<Person>();
-            List<FaceDetectResponse> detectResult = await _faceAPIService.DetectFaces(image);
+            List<FaceDetectResponse> detectResult = await _faceAPIService.DetectFacesAsync(image);
             if (detectResult != null && detectResult.Count > 0)
             {
                 foreach(FaceDetectResponse x in detectResult)
@@ -50,12 +50,12 @@ namespace WebApplication1.Services
                     string faceID = x.FaceId;
                     if (!string.IsNullOrEmpty(faceID))
                     {
-                        List<FaceIdentifyResponse> identifyResult = await _faceAPIService.Identify(faceID, _groupID, 1);
+                        List<FaceIdentifyResponse> identifyResult = await _faceAPIService.IdentifyAsync(faceID, _groupID, 1);
                         if (identifyResult != null && identifyResult.Count > 0 && identifyResult[0].Candidates.Count > 0)
                         {
                             string personId = identifyResult[0].Candidates[0].PersonId;
                             double confidence = identifyResult[0].Candidates[0].Confidence;
-                            FaceAPIPersonResponse person = await _faceAPIService.GetPerson(_groupID, personId);
+                            FaceAPIPersonResponse person = await _faceAPIService.GetPersonAsync(_groupID, personId);
                             recognizedPersons.Add(_personRepository.GetByFaceAPIID(personId));
                         }
                     }
