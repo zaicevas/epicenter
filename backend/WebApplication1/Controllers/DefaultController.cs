745d6b11fafd8949e7b715e7b8567a8afc758faf
Tomas Zaicevas <tozaicevas@gmail.com>
2018-11-23 02:40:13 +0200

    single exception + thrown exception in plate api

--- backend/WebApplication1/Controllers/DefaultController.cs
+++ backend/WebApplication1/Controllers/DefaultController.cs
@@ -16,8 +16,8 @@ namespace WebApplication1.Controllers
         {
             byte[] imageArray = System.IO.File.ReadAllBytes(@"C:\Users\ferN\plate_testing\FNN883.jpg");
             string base64ImageRepresentation = Convert.ToBase64String(imageArray);
-            PlateResponse plateResponse = new PlateService().Recognize(base64ImageRepresentation);
-            System.Diagnostics.Debug.WriteLine(plateResponse.Message);
+            //PlateResponse plateResponse = new PlateService().Recognize("noob");
+            //System.Diagnostics.Debug.WriteLine(plateResponse.Message);
             return Ok("ALL GOOD BOY");
         }
     }
