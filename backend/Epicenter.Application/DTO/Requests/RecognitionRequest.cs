ad163a1d4489b862cd643ac5883dc3d2dd7a61c8
Valdas Rakutis <valdas@rakutis.lt>
2018-12-09 12:45:22 +0200

    Change the request format.

---
+++ backend/Epicenter.Application/DTO/Requests/RecognitionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Epicenter.Application.DTO.Requests
+{
+    public class RecognitionRequest
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string ImageBase64 { get; set; }
+        public bool FoundPlate { get; set; }
+        public bool FoundFace { get; set; }
+    }
+}
