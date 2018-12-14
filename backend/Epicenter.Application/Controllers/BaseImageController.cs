6286a8944243b0e5eb731a53dfc16d1c24580396
Žygimantas Stongvilas <stongvilas.zygimantas@gmail.com>
2018-12-14 20:22:45 +0200

    BaseImage Controller and Service

---
+++ backend/Epicenter.Application/Controllers/BaseImageController.cs
@@ -0,0 +1,37 @@
+﻿using Epicenter.Domain.Models.DTO;
+using Epicenter.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Epicenter.Application.Controllers
+{
+    [Route("api/missingmodels/baseimages")]
+    [ApiController]
+    public class BaseImageController : ControllerBase
+    {
+        private readonly BaseImageService _baseImageService;
+
+        public BaseImageController(BaseImageService baseImageService)
+        {
+            _baseImageService = baseImageService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public IActionResult Get()
+        {
+            List<MissingModelBaseImage> missingModelsBaseImages;
+            try
+            {
+                missingModelsBaseImages = _baseImageService.GetAllBaseImages();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { Error = ex.Message });
+            }
+            return Ok(missingModelsBaseImages.ToArray());
+        }
+    }
+}
---
+++ backend/Epicenter.Domain/Services/BaseImageService.cs
@@ -0,0 +1,45 @@
+﻿using Epicenter.Domain.Abstract;
+using Epicenter.Domain.Models;
+using Epicenter.Domain.Models.Abstract;
+using Epicenter.Domain.Models.DTO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epicenter.Domain.Services
+{
+    public class BaseImageService
+    {
+        private readonly IPersonRepository _personRepository;
+        private readonly IPlateRepository _plateRepository;
+
+        public BaseImageService(IPersonRepository personRepository, IPlateRepository plateRepository)
+        {
+            _personRepository = personRepository;
+            _plateRepository = plateRepository;
+        }
+
+        public List<MissingModelBaseImage> GetAllBaseImages()
+        {
+            return GetBaseImages<Person>().Concat(GetBaseImages<Plate>()).ToList();
+        }
+
+        public List<MissingModelBaseImage> GetBaseImages<T>() where T : MissingModel
+        {
+            IEnumerable<MissingModel> missingModels;
+            if (typeof(T) == typeof(Person))
+                missingModels = _personRepository.GetAll();
+            else
+                missingModels = _plateRepository.GetAll();
+            List<MissingModelBaseImage> baseImages = new List<MissingModelBaseImage>();
+            missingModels.ToList().ForEach(missingModel =>
+            {
+                baseImages.Add(new MissingModelBaseImage()
+                {
+                    Id = missingModel.Id,
+                    BaseImage = missingModel.BaseImage
+                });
+            });
+            return baseImages;
+        }
+    }
+}
