7ddd11fa1ca83fce6319c0e6069cfc9d147b8f69
Žygimantas Stongvilas <stongvilas.zygimantas@gmail.com>
2018-12-16 01:00:45 +0200

    Updated database

--- backend/Epicenter.Persistence/Migrations/20181215225754_TimestampLocation.Designer.cs
+++ backend/Epicenter.Persistence/Migrations/20181215225754_TimestampLocation.Designer.cs
@@ -4,14 +4,16 @@ using Epicenter.Persistence.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Epicenter.Persistence.Migrations
 {
     [DbContext(typeof(EpicenterDbContext))]
-    partial class EpicenterContextModelSnapshot : ModelSnapshot
+    [Migration("20181215225754_TimestampLocation")]
+    partial class TimestampLocation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
