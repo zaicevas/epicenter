4c506f9e125cfbc3e448dd5908abe9d33cb091b2
Žygimantas Stongvilas <stongvilas.zygimantas@gmail.com>
2018-12-16 03:22:34 +0200

    Saving Smile with Timestamp

--- backend/Epicenter.Domain/Models/DTO/TimestampResponse.cs
+++ backend/Epicenter.Domain/Models/DTO/TimestampResponse.cs
@@ -6,6 +6,7 @@
         public string DateAndTime { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public double Smile { get; set; }
         public MissingModelResponse MissingModel { get; set; }
     }
 }
